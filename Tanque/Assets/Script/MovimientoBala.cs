e6e2af4dae9a62685b2f40d14ff7ddb831d425ce
Giancarlo <20161333@aloe.ulima.edu.pe>
2019-12-11 11:05:14 -0500

    Todo

---
+++ Tanque/Assets/Script/MovimientoBala.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoBala : MonoBehaviour
+{
+    private float velocidad = 20f;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+
+    }
+
+
+
+    void Update()
+    {
+
+        transform.position += new Vector3(0,0 , Time.deltaTime * velocidad);
+        if (transform.position.z > 50)
+        {
+            Destroy(gameObject);
+        };
+    }
+
+}
---
+++ Tanque/Assets/Script/MovimientoTanque.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoTanque : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float fuerzaIzqDer;
+    private float tiempoVida = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translationH = Input.GetAxis("Horizontal");
+        float translationV = Input.GetAxis("Vertical");
+        transform.Translate(0, 0, translationV * Time.deltaTime * 10);
+        transform.Translate(translationH * Time.deltaTime * 10, 0, 0);
+        Disparar();
+    }
+
+    private void Disparar()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            tiempoVida -= Time.deltaTime;
+            if (tiempoVida <= 0)
+            {
+                CrearBala();
+                tiempoVida = 0.2f;
+            }
+        }
+    }
+
+    private void CrearBala()
+    {
+        Instantiate(Resources.Load("Bala"), new Vector3(transform.position.x+2.6f, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+
+
+}
